8f83ffdebacb093bb8e02d9faabe173b4daf6d9f
Suvid Datta <suvid.datta@gmail.com>
2022-01-08 11:05:07 +0530

    Testing run.sh, dont mind me

--- testscripts/normal.py
+++ testscripts/normal.py
@@ -8,3 +8,4 @@ print(requests.post('http://127.0.0.1:5000/remove/hmmm',json={'a':1,'b':2},auth=
 print(requests.post('http://127.0.0.1:5000/getdata/hmmm',auth=('admin','admin')).text)
 #print(requests.post('http://127.0.0.1:5000/delete/hmmm',auth=('admin','admin')).text)
 print(requests.post('http://127.0.0.1:5000/save',auth=('admin','admin')).text)
+print("OK")
