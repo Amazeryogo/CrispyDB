230f9388c51a4933722b1dd9d4ccc372475cc227
Suvid Datta <suvid.datta@gmail.com>
2022-01-08 19:52:18 +0530

    ready for v1.1

--- testscripts/normal.py
+++ testscripts/normal.py
@@ -2,7 +2,7 @@ import requests
 
 print(requests.post('http://127.0.0.1:5000/create/hmmm',auth=('admin','admin')).text)
 print(requests.post('http://127.0.0.1:5000/getdata/hmmm',auth=('admin','admin')).text)    
-print(requests.post('http://127.0.0.1:5000/add/hmmm',json={'a':1,'b':2},auth=('admin','admin')).text)
+print(requests.post('http://127.0.0.1:5000/add/hmmm',json="{'a':1,'b':2}",auth=('admin','admin')).text)
 print(requests.post('http://127.0.0.1:5000/getdata/hmmm',auth=('admin','admin')).text)
 #print(requests.post('http://127.0.0.1:5000/remove/hmmm',json={'a':1,'b':2},auth=('admin','admin')).text)
 #print(requests.post('http://127.0.0.1:5000/getdata/hmmm',auth=('admin','admin')).text)
