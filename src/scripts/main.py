0dab75272d8e135dad513da9d9c540c77d0c4ef4
Suvid Datta <suvid.datta@gmail.com>
2022-02-23 21:57:32 +0530

    some changes

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,12 +1,12 @@
 import requests as rq
 
-exampletoken = "1e533624-1ea6-4106-9f61-a557cc917632"
+token = "1e533624-1ea6-4106-9f61-a557cc917632"
 
 print(rq.get('http://0.0.0.0:5000/').text)
-print(rq.get('http://0.0.0.0:5000/create/collection?exampletoken=' + exampletoken).text)
-print(rq.get('http://0.0.0.0:5000/getdata/collection?exampletoken=' + exampletoken).text)
-print(rq.post('http://0.0.0.0:5000/add/collection?exampletoken=' + exampletoken,json={'name':'test','data':'test'}).text)
-print(rq.get('http://0.0.0.0:5000/getdata/collection?exampletoken=' + exampletoken).text)
-print(rq.post('http://0.0.0.0:5000/removefrom/collection?exampletoken=' + exampletoken,json={'name':'test','_crispy-id':1}).text)
-print(rq.get('http://0.0.0.0:5000/getdata/collection?exampletoken=' + exampletoken).text)
-print(rq.post('http://0.0.0.0:5000/delete/collection?exampletoken=' + exampletoken).text)
+print(rq.get('http://0.0.0.0:5000/create/collection?token=' + token).text)
+print(rq.get('http://0.0.0.0:5000/getdata/collection?token=' + token).text)
+print(rq.post('http://0.0.0.0:5000/add/collection?token=' + token,json={'name':'test','data':'test'}).text)
+print(rq.get('http://0.0.0.0:5000/getdata/collection?token=' + token).text)
+print(rq.post('http://0.0.0.0:5000/removefrom/collection?token=' + token,json={'name':'test','_crispy-id':1}).text)
+print(rq.get('http://0.0.0.0:5000/getdata/collection?token=' + token).text)
+print(rq.post('http://0.0.0.0:5000/delete/collection?token=' + token).text)
